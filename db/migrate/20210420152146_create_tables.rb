f4cd314aa5411dfd49a8047d4b26137e6380cb7b
ayman <aymanjaber2012@gmail.com>
2021-08-19 13:19:56 +0300

    Add preliminary migration file

--- db/migrate/20210420152146_create_tables.rb
+++ db/migrate/20210420152146_create_tables.rb
@@ -15,32 +15,27 @@ class CreateTables < ActiveRecord::Migration[6.0]
       t.string :last_sign_in_ip
       t.timestamps null: false
     end
-
     add_index :admin_users, :reset_password_token, unique: true
-
     create_table :refresh_auth_tokens do |t|
       t.timestamps null: false
     end
-
     create_table :blacklisted_auth_tokens do |t|
       t.timestamps null: false
     end
 
+
     create_table :users do |t|
-      ## Database authenticatable
       t.string :email,              null: false, default: ""
       t.string :username,              null: false, default: ""
-      t.string :encrypted_password, null: false, default: ""
 
-      ## Recoverable
+      # The few lines below are used by devise
+      t.string :encrypted_password, null: false, default: ""
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
-
-      ## Rememberable
       t.datetime :remember_created_at
+
       t.timestamps null: false
     end
-
     add_index :users, :email,                unique: true
     add_index :users, :username,             unique: true
     add_index :users, :reset_password_token, unique: true
@@ -48,12 +43,36 @@ class CreateTables < ActiveRecord::Migration[6.0]
 
     create_table :books do |t|
       t.integer :user_id
-      t.text :content
+      t.text :title
+      t.text :author
+      t.text :description
     
       t.timestamps
     end
-
     add_index :books, :user_id
+
+
+    create_table :reviews do |t|
+      t.integer :user_id
+      t.integer :book_id
+      t.text :title
+      t.text :description
+      t.integer :rate
+      t.timestamps
+    end
+    add_index :reviews, :book_id
+    add_index :reviews, :user_id
+    
+
+    create_table :friendships do |t|
+      t.references :user, index: true, null: false, foreign_key: true
+      t.integer :friend_id, null: false
+      t.boolean :confirmed
+
+      t.timestamps
+    end
+    add_foreign_key :friendships, :users, column: :friend_id, foreign_key: true
+
 
   end
 end
